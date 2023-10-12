86098e35239e5ddd5e2f288caba7901105e88de0
james yates <yates_7@hotmail.com>
2023-10-12 14:25:27 +0100

    console logs added to main ts for bug tracking on removeItem function

--- src/templates/ListTemplates.ts
+++ src/templates/ListTemplates.ts
@@ -50,7 +50,9 @@ export default class ListTemplates implements DOMList {
             li.append(button)
 
             button.addEventListener('click', () => {
+                console.log(fullList)
                 fullList.removeItem(item.id)
+                console.log(fullList)
                 this.render(fullList)
             })
 
